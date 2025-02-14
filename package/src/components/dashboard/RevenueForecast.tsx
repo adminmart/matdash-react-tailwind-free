ee37d96d7d68b074cc017132002dce9d2f408528
wrappixelTeam <wrappixelteam.2016@gmail.com>
2025-02-14 15:00:17 +0530

    updated

--- package/src/components/dashboard/RevenueForecast.tsx
+++ package/src/components/dashboard/RevenueForecast.tsx
@@ -9,11 +9,11 @@ const RevenueForecast = () => {
 
   const getChartData = (period: string) => {
     switch (period) {
-      case "April 2024":
+      case "April 2025":
         return {
           series: [
             {
-              name: "2024",
+              name: "2025",
               data: [2.5, 3.0, 2.8, 3.2, 2.9, 3.1, 2.7, 2.8, 3.0],
             },
             {
@@ -22,11 +22,11 @@ const RevenueForecast = () => {
             },
           ],
         };
-      case "May 2024":
+      case "May 2025":
         return {
           series: [
             {
-              name: "2024",
+              name: "2025",
               data: [2.7, 2.9, 2.6, 3.1, 3.0, 2.8, 2.9, 3.2, 3.1],
             },
             {
@@ -35,11 +35,11 @@ const RevenueForecast = () => {
             },
           ],
         };
-      case "June 2024":
+      case "June 2025":
         return {
           series: [
             {
-              name: "2024",
+              name: "2025",
               data: [3.0, 3.2, 3.1, 3.5, 3.4, 3.3, 3.2, 3.4, 3.6],
             },
             {
@@ -53,7 +53,7 @@ const RevenueForecast = () => {
         return {
           series: [
             {
-              name: "2024",
+              name: "2025",
               data: [1.2, 2.7, 1.0, 3.6, 2.1, 2.7, 2.2, 1.3, 2.5],
             },
             {
@@ -149,9 +149,9 @@ const RevenueForecast = () => {
           required
         >
           <option value="This Week">This Week</option>
-          <option value="April 2024">April 2024</option>
-          <option value="May 2024">May 2024</option>
-          <option value="June 2024">June 2024</option>
+          <option value="April 2025">April 2025</option>
+          <option value="May 2025">May 2025</option>
+          <option value="June 2025">June 2025</option>
         </Select>
       </div>
 
